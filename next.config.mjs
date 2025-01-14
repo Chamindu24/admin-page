d0fa3d384d707417e0d550d6e230ee52071ead10
hasithasandun <hasiofficial2002@gmail.com>
2025-01-15 04:06:06 +0530

    Enhance Next.js configuration for serverless deployment and exclude canvas dependency from server-side build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    target: 'serverless', // Ensures the app is deployed as a serverless function
+    webpack(config, { isServer }) {
+      // Disable the server-side build of the `canvas` dependency (if you still have it)
+      if (isServer) {
+        config.externals = ['canvas', ...config.externals];
+      }
+      return config;
+    },
+   
+  };
+  
+  export default nextConfig;
+  
