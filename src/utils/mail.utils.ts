e5a5b3bcec7a952591988b95e5ead01e4c770e38
hasithasandun <hasiofficial2002@gmail.com>
2025-01-15 01:44:36 +0530

    Update mail transport configuration to use secure connection for SMTP port 465

--- src/utils/mail.utils.ts
+++ src/utils/mail.utils.ts
@@ -5,7 +5,7 @@ import SMTPTransport from 'nodemailer/lib/smtp-transport';
 const transport = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
   port: Number(process.env.MAIL_PORT),
-  secure: process.env.NODE_ENV !== 'development', // Use secure connection if not in development
+  secure: process.env.SMTP_PORT === "465", // Use secure for port 465 // Use secure connection if not in development
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASSWORD,
